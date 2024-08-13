33edefed02bf74a95e7cc9bc045956d5616026db
Katalogo262f <77251259+Katalogo@users.noreply.github.com>
2024-08-13 15:49:51 +0000

    Input is controlled & uncontrolled

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -7,7 +7,10 @@ export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> 
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, onKeyDown, customOnChange, value, ...props }, ref) => {
+  ({ className, type, onKeyDown, customOnChange, value, onChange, ...props }, ref) => {
+    const [internalValue, setInternalValue] = React.useState(value || "");
+    const isControlled = value !== undefined;
+
     const keyMap: { [key: string]: string } = {
       q: "a", w: "b", e: "c", r: "d", t: "e", y: "f", u: "g", i: "h", o: "i", p: "j", a: "k", s: "l", d: "m",
       f: "n", g: "o", h: "p", j: "q", k: "r", l: "s", z: "t", x: "u", c: "v", v: "w", b: "x", n: "y", m: "z",
@@ -21,15 +24,16 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     };
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-      console.log("child");
-      customOnChange?.(event.target.value);
+      const newValue = event.target.value;
+      isControlled ? customOnChange?.(newValue) : setInternalValue(newValue);
+      onChange?.(event);
     };
 
     const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
       if (keyMap.hasOwnProperty(event.key)) {
         event.preventDefault();
-        const newValue = (value as string) + keyMap[event.key];
-        customOnChange?.(newValue);
+        const newValue = (isControlled ? value : internalValue) + keyMap[event.key];
+        isControlled ? customOnChange?.(newValue) : setInternalValue(newValue);
       }
     };
 
@@ -44,7 +48,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
-        value={value}
+        value={isControlled ? value : internalValue}
         onChange={handleChange}
         onKeyDown={mergedOnKeyDown}
         onPaste={preventCopyPaste}
