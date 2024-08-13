0fe4c0dc4627e21b593604a48d38489934a43bce
Katalogo262f <77251259+Katalogo@users.noreply.github.com>
2024-08-13 11:47:48 +0000

    keys case insensitive

--- main_folder/components/ui/input.tsx
+++ main_folder/components/ui/input.tsx
@@ -8,11 +8,12 @@ export interface InputProps
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, onKeyDown, onChange, value, ...props }, ref) => {
-    const [inputvalue, setInputvalue] = useState<string>("");
     //keymap
     const keyMap: { [key: string]: string } = {
       q: "a", w: "b", e: "c", r: "d", t: "e", y: "f", u: "g", i: "h", o: "i", p: "j", a: "k", s: "l", d: "m",
       f: "n", g: "o", h: "p", j: "q", k: "r", l: "s", z: "t", x: "u", c: "v", v: "w", b: "x", n: "y", m: "z",
+      Q: "A", W: "B", E: "C", R: "D", T: "E", Y: "F", U: "G", I: "H", O: "I", P: "J", A: "K", S: "L", D: "M",
+      F: "N", G: "O", H: "P", J: "Q", K: "R", L: "S", Z: "T", X: "U", C: "V", V: "W", B: "X", N: "Y", M: "Z"
     };
 
     const preventCopyPaste = (e: ClipboardEvent<HTMLInputElement>) => {
