1abac3dbe47632279fa3619ee0b4442f95a88a96
Saad <76481274+saad6101@users.noreply.github.com>
2024-08-14 15:32:16 +0000

    Made Main Website

---
+++ app/pages/Website/stats.tsx
@@ -0,0 +1,64 @@
+const stats = [
+    {
+        name: "Vinicius",
+        imageSrc: "/assets/vinicius.jpg",
+        width: 200, // Smaller width
+        height: 300, // Smaller height
+        stats: {
+            Goals: 26,
+            Assists: 12,
+            PassingAccuracy: "79%",
+            Position: "LW",
+            MatchesPlayed: 48,
+            Accolades: "UCL, \n LaLiga",
+            Nationality: "Brazilian",
+            Club: "Real Madrid",
+            DribblePercentage: "130/250",
+            AverageRating: "7.38",
+            DefensiveDuties: "Good",
+            MinutesPlayed: 3670
+        }
+    },
+    {
+        name: "Bellingham",
+        imageSrc: "/assets/bellingham.jpg",
+        width: 200,
+        height: 300, 
+        stats: {
+            Goals: 26,
+            Assists: 14 ,
+            PassingAccuracy: "89.92%",
+            Position: "CAM",
+            MatchesPlayed: 51,
+            Accolades: "UCL, \n LaLiga, \n EURO's finalist",
+            Nationality: "English",
+            Club: "Real Madrid",
+            DribblePercentage: "87/127",
+            AverageRating: "8.05",
+            DefensiveDuties: "50%",
+            MinutesPlayed: 4352
+        },
+    },
+    {
+        name: "Rodri",
+        imageSrc: "/assets/rodri.jpg",
+        width: 200, // Smaller width
+        height: 300, // Smaller height
+        stats: {
+            Goals: 12,
+            Assists: 13,
+            PassingAccuracy: "92.38%",
+            Position: "CDM",
+            MatchesPlayed: 56,
+            Accolades: "English Premier League,\n Euro,\n FA Cup, \n Carabao Cup Finalist, \n Community Shield Finalist, \n UCL Semi-Finalist",
+            Nationality: "Spanish",
+            Club: "Manchester City",
+            DribblePercentage: "41/55",
+            AverageRating: "8.03",
+            DefensiveDuties: "70%",
+            MinutesPlayed: 4818
+        },
+    }
+];
+
+export default stats;
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "",
-    output: "export",
-    reactStrictMode: true,
+  basePath: "",
+  output: "export",
+  reactStrictMode: true,
+  images: {
+      unoptimized: true,
+  },
 };
 
 export default nextConfig;
